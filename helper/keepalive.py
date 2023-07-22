e196fc0a4b61107a883d553c361a7f5f8e3d24d5
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-07-22 14:21:44 +0530

    Add files via upload

---
+++ helper/keepalive.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2023 DX-MODS
+#Licensed under the  AGPL-3.0 License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
+
+import asyncio
+import logging
+import aiohttp
+import traceback
+from config import Config
+
+
+async def ping_server():
+    sleep_time = Config.PING_INTERVAL
+    while True:
+        await asyncio.sleep(sleep_time)
+        try:
+            async with aiohttp.ClientSession(
+                timeout=aiohttp.ClientTimeout(total=10)
+            ) as session:
+                async with session.get(Config.PING_WEB) as resp:
+                    logging.info("Pinged server with response: {}".format(resp.status))
+        except TimeoutError:
+            logging.warning("Couldn't connect to the site URL..!")
+        except Exception:
+            traceback.print_exc()
---
+++ helper/txt.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2023 DX-MODS
+#Licensed under the  AGPL-3.0 License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN
+#if you use our codes try to donate here https://www.buymeacoffee.com/ziyankp
+
+class Txt(object):
+
+    START_TXT = """<b>Hᴀɪ {} 👋,
+𝚃𝚑𝚒𝚜 𝚒𝚜 𝚊 𝚜𝚒𝚖𝚙𝚕𝚎 𝚝𝚎𝚕𝚎𝚐𝚛𝚊𝚙𝚑 𝚗𝚘𝚝 𝚝𝚘 𝚞𝚙𝚕𝚘𝚊𝚍 𝚒𝚖𝚊𝚐𝚎 𝚊𝚗𝚍 𝚐𝚒𝚏 𝚊𝚗𝚍 𝚟𝚒𝚍𝚎𝚘𝚜 𝚞𝚗𝚍𝚎𝚛 𝟻𝙼𝙱
+𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝚒𝚜 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚍 𝙱𝚢 𝚉𝚒𝚢𝚊𝚗
+
+@dxziyan 💞</b>"""
+
+    ABOUT_TXT = """
+╭───────────⍟
+├🤖 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
+├👑 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁𝚂 : <a href=https://t.me/dxziyan>𝚉𝙸𝚈𝙰𝙽</a> 
+├👨‍💻 𝙿𝚁𝙾𝙶𝚁𝙰𝙼𝙴𝚁 : <a href=https://github.com/DX-MODS>𝙳𝚇 𝙼𝙾𝙳𝚂</a>
+├📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://github.com/pyrogram>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+├✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
+├💾 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾𝙳𝙱</a>
+├📊 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚄𝚂 : v2.1.90              
+╰───────────────⍟
+                                """
+    HELP_TXT = """
+🌌 <b><u>HOW TO USE THIS BOT</u></b>
+  
+•> /start a bot and send link to bot for download instagram videos.
+•> /this bot support currently reels videos and post.
+"""
+
+#⚠️ don't remove our credits 🙏😢😢
+    DEV_TXT = """
+<b><u>Source Code </b></u> 
+
+» 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘 : <a href=https://github.com/DX-MODS/Telegraph-Bot</a>
+» 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 : <a href=https://youtube.com/@DX-MODS</a>
+"""
